2a9e16a5cce9ce0310541a5a3181c9de8898c94d
Z3r0w0rm <zeroworm@outlook.com>
2015-11-13 18:05:46 +0900

    EncryptModule

---
+++ SafeFolder/MainWindow.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SafeFolder
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SeedLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seed:";
+            // 
+            // SeedLabel
+            // 
+            this.SeedLabel.AutoSize = true;
+            this.SeedLabel.Location = new System.Drawing.Point(49, 51);
+            this.SeedLabel.Name = "SeedLabel";
+            this.SeedLabel.Size = new System.Drawing.Size(17, 12);
+            this.SeedLabel.TabIndex = 4;
+            this.SeedLabel.Text = "--";
+            this.SeedLabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SeedLabel_MouseMove);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 21);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Verify:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 99);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SeedLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainWindow";
+            this.Text = "Builder";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label SeedLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ SafeFolder/MainWindow.cs
@@ -0,0 +1,188 @@
+﻿/* Coded By PhilipMo(ZeroWorm) */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.IO;
+
+namespace SafeFolder
+{
+    public partial class MainWindow : Form
+    {
+        Random r = new Random();
+        string cMap = "1234567890!@#$%^&*()-=_+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,./;'[]<>?:{}|";
+        string varGen = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string GenStr(int len, string map)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < len; i++)
+                sb.Append(map[r.Next(0, map.Length - 1)]);
+            return sb.ToString();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            SeedLabel.Text = GenStr(30, cMap);
+        }
+
+        private void SeedLabel_MouseMove(object sender, MouseEventArgs e)
+        {
+            SeedLabel.Text = GenStr(30, cMap);
+        }
+
+        private string ShaHash(string strtohash)
+        {
+            using(SHA512 _sha = new SHA512Managed())
+            {
+                byte[] hashedBytes = _sha.ComputeHash(Encoding.UTF8.GetBytes(strtohash));
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashedBytes)
+                    sb.Append(b.ToString("x2"));
+                return sb.ToString();
+            }
+        }
+
+        private string EncryptString(string obf8_, string encBase, string salt)
+        {
+            byte[] obf14_ = System.Text.Encoding.UTF8.GetBytes(obf8_);
+            using (Rijndael obf9_ = new RijndaelManaged())
+            {
+                PasswordDeriveBytes obf16_ = new PasswordDeriveBytes(encBase, System.Text.Encoding.UTF8.GetBytes(salt));
+                obf9_.Key = obf16_.GetBytes(obf9_.KeySize / 8);
+                obf9_.IV = obf16_.GetBytes(obf9_.BlockSize / 8);
+                using (MemoryStream obf15_ = new MemoryStream())
+                {
+                    using (CryptoStream obf17_ = new CryptoStream(obf15_, obf9_.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        obf17_.Write(obf14_, 0, obf14_.Length);
+                    }
+                    return Convert.ToBase64String(obf15_.ToArray());
+                }
+            }
+        }
+
+
+        public bool Compile(string code, string compileTo)
+        {
+            CSharpCodeProvider compiler = new CSharpCodeProvider();
+            CompilerParameters parmiters = new CompilerParameters(new string[] { "mscorlib.dll", "System.Core.dll", "System.dll" });
+            parmiters.GenerateExecutable = true;
+            parmiters.CompilerOptions = "/target:exe /platform:x86";
+            parmiters.OutputAssembly = compileTo;
+            CompilerResults r = compiler.CompileAssemblyFromSource(parmiters, code);
+            return !(r.Errors.Count > 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Enter a password.");
+                return;
+            }
+            if (!textBox2.Text.Equals(textBox1.Text))
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+            CSharpCodeProvider compiler = new CSharpCodeProvider();
+            CompilerParameters parmiters = new CompilerParameters(new string[] { "mscorlib.dll", "System.Core.dll", "System.dll" });
+            parmiters.GenerateExecutable = true;
+            parmiters.CompilerOptions = "/target:exe /platform:x86";
+            HashSet<string> varnameVerify = new HashSet<string>();
+
+            string s = SafeFolder.Properties.Resources.SafeFolderModule;
+            int i = 0;
+            while (s.Contains(string.Format("obf{0}_", i)))
+            {
+                string gname = GenStr(10, varGen);
+                while(!varnameVerify.Add(gname))
+                    gname = GenStr(10, varGen);
+                s = s.Replace(string.Format("obf{0}_", i), gname);
+                i++;
+            }
+
+            string[] FuncEnc = new string[] { "obf_str_enc_func_" };
+            foreach (string funcS in FuncEnc)
+            {
+                string gname = GenStr(10, varGen);
+                while (!varnameVerify.Add(gname))
+                    gname = GenStr(10, varGen);
+                s = s.Replace(funcS, gname);
+            }
+
+            string DescFuncName = GenStr(10, varGen);
+            while (!varnameVerify.Add(DescFuncName))
+                DescFuncName = GenStr(10, varGen);
+            s = s.Replace("obf_str_dec_func_", DescFuncName);
+
+            Dictionary<string, string> StrEnc = new Dictionary<string, string>()
+            {
+                {"[PWPROMPT]", "Password: "},
+                {"[WRONGPROMPT]", "Wrong."},
+                {"[DIRDATA]", "dirdata"},
+                {"[ENTERTODECRYPT]", "Press enter to decrypt..."},
+                {"[ENTERTOENCRYPT]", "Press enter to encrypt..."},
+                {"[DONE]", "Done."},
+                {"[BLACKSLASHB]", "\\b"},
+                {"[PWMASK]", "*"},
+                {"[ENCRYPTEDOUTPUT]","Encrypted: {0}"},
+                {"[DECRYPTEDOUTPUT]","Decrypted: {0}"},
+                {"[ERRORPARAMS]", "({0}) {1}"},
+                {"[TIMES2]", "x2"},
+                {"[FAILEDDELETEDIRDATA]", "Failed to delete dirdata, please manualy delete."},
+                {"[LOADEDFILENAME]", "Loaded file name: {0}"},
+                {"[CREDIT]", "SafeFolder by PhilipMo(ZeroWorm)"},
+                {"[PWHASH]", ShaHash(textBox1.Text)}
+            };
+
+            foreach(var strDic in StrEnc)
+            {
+                string salt = GenStr(5, cMap);
+                string enc = EncryptString(strDic.Value, salt, SeedLabel.Text);
+                string replace = string.Format("{0}(\"{1}\", \"{2}\")", DescFuncName, enc, salt);
+                string from = string.Format("\"{0}\"", strDic.Key);
+                s = s.Replace(from, replace);
+            }
+
+            s = s.Replace("[SALT]", SeedLabel.Text);
+            //s = s.Replace("[PWHASH]", ShaHash(textBox1.Text));
+
+            using(SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Exe|*.exe";
+                if(sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    parmiters.OutputAssembly = sfd.FileName;
+                    CompilerResults r = compiler.CompileAssemblyFromSource(parmiters, s);
+                    if (r.Errors.Count > 0)
+                    {
+                        foreach(CompilerError er in r.Errors)
+                        {
+                            Console.WriteLine("{0} {1}", er.Line, er.ErrorText);
+                        }
+                        MessageBox.Show("Failed");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Built");
+                    }
+                }
+            }
+
+        }
+    }
+}
